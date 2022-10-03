7d1414c396d32a6ad5fcdf99a9e9bdf5ef271670
Manoj <mrout02@gmail.com>
2022-10-03 20:36:29 +0530

    Second Commit

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);.
 	}
 }
